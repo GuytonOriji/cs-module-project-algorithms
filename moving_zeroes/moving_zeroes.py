67d1d82a5a25a6866cbab41104be3a9207c8b107
tgc <codingviewdmv@gmail.com>
2020-06-18 02:58:48 -0400

    mvp so far will coninues later

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,31 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+
+def moving_zeroes(arr):
+    zerovalue_item =0
+      
+    for i in range(len(arr)):
+        # remove zero value items and append them at the end
+        if arr[i]==zerovalue_item:       
+            arr.remove(zerovalue_item)
+            print(len(arr))
+            arr.append(zerovalue_item)        
+            print(arr)
+           # if there are not any , return array             
+        else:
+            print(arr)                 
+            
+    return arr
+
+    
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,23 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+def single_number(arr):
+
+    # Your code here
+    # gathering the values that have duplicates then removing
+    # them and then the only 1 left would be the answer to
+    # this question XD
+  
+    from collections import Counter
+     
+
+    dict_arr = Counter(arr)
+    print(dict_arr)
+    result = [k for k, v in dict_arr.items() if v<2]
+    print(result)
+    print(type(result))  
+    return result[0]    
 
 
 if __name__ == '__main__':
